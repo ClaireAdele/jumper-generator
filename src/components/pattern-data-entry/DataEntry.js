c49f419e167385768bd4b11030af137eabf2c4f4
Claire Castanet <clairecastanet@MASTAFF-Claire-Castanet-Q14CH4R1PW.local>
2023-11-17 17:49:28 +0000

    Added the data entry page, built some of the How To Take your Measurements stuff too

--- src/components/pattern-data-entry/DataEntry.js
+++ src/components/pattern-data-entry/DataEntry.js
@@ -1,17 +1,66 @@
 import "../../App.css";
+import HowToTakeMeasurements from "./HowToTakeMeasurements";
+import "./DataEntry.css"
 
 const DataEntry = () => {
-    // chest measurement + ease
-    // shoulder measurements
-    //pattern repeat?? Could do with that too maybe?
-    //gauge of yarn in rows 
-    // body length, from bottom of jumper to armhole
-    //from armhole to bottom of neckline
     return (
       <div className="pageBackground">
-        <div className="pageShaper"></div>
+        <div className="pageShaper">
+          <div id="data-entry-page">
+            <div id="data-entry-container">
+              <div id="measurements-form-container">
+                <h3>Measurements Entry:</h3>
+                <table id="prices-table">
+                  <tr>
+                    <td>Chest cirumference</td>
+                    <td>
+                      <input type="number"></input>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Bottom of jumper to armpit</td>
+                    <td>
+                      <input type="number"></input>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Armpit line to neckline</td>
+                    <td>
+                      <input type="number"></input>
+                    </td>
+                  </tr>
+
+                  <tr>
+                    <td>Arm length - pit to wrist</td>
+                    <td>
+                      <input type="number"></input>
+                    </td>
+                  </tr>
+                </table>
+                <button>Submit</button>
+              </div>
+              <div id="ease-selection-container">
+                <h3>Desired Fit</h3>
+                <h4>Pick a standard amount of ease:</h4>
+                <button className="ease-selector-button">Fitted</button>
+                <button className="ease-selector-button">Standard</button>
+                <button className="ease-selector-button">Loose</button>
+                <button className="ease-selector-button">Oversized</button>
+                <h4>Or add a custom amount:</h4>
+                <input type="number"></input>
+                <button>Submit</button>
+              </div>
+            </div>
+            <h2>How to take your measurements</h2>
+            <HowToTakeMeasurements />
+          </div>
+        </div>
       </div>
     );
 };
 
 export default DataEntry;
+
+
