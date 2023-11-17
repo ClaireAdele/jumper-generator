0979c41d07df89acdf1489a69e24dbb5381fcbc6
Claire Castanet <claire.castanet@outlook.com>
2023-11-17 17:55:12 +0000

    testing that the user committing is the right one

--- src/components/pattern-data-entry/HowToTakeMeasurements.js
+++ src/components/pattern-data-entry/HowToTakeMeasurements.js
@@ -50,7 +50,7 @@ const HowToTakeMeasurements = () => {
       </div>
       <div className="instructions-card-right">
         <p className="measurement-instructions-right">
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          "Lorem ips um dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat. Duis aute irure dolor in
